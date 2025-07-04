fab66ba4072f25d12e09bdb30f2d0ad9f8054889
samirtelegrambot <samirlaskar43@gmail.com>
2025-07-05 01:14:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import logging
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
+from telegram.ext import (
+    ApplicationBuilder, CommandHandler, MessageHandler,
+    filters, CallbackQueryHandler
+)
+from telegram import Update
 from config.manager import ConfigManager
 from handlers.admin import admin_menu, add_admin, remove_admin, bot_stats
-from handlers.channel import channel_menu, add_channel_prompt, handle_channel_input, manage_channel, remove_channel, back_to_channels
+from handlers.channel import (
+    channel_menu, add_channel_prompt, handle_channel_input,
+    manage_channel, remove_channel, back_to_channels
+)
 from handlers.batch import batch_menu, collect_message, clear_batch, show_batch
-from handlers.schedule import schedule_menu, view_scheduled_posts, schedule_new_post_prompt, receive_scheduled_message, receive_scheduled_time
+from handlers.schedule import (
+    schedule_menu, view_scheduled_posts, schedule_new_post_prompt,
+    receive_scheduled_message, receive_scheduled_time
+)
 from handlers.post import post_menu, preview_post, execute_post
 from handlers.menu import start, help_command, back_to_main_menu
-from handlers.settings import settings_menu, set_delay_prompt, receive_delay_input, set_retry_prompt, receive_retry_input, set_footer_prompt, receive_footer_input
+from handlers.settings import (
+    settings_menu, set_delay_prompt, receive_delay_input,
+    set_retry_prompt, receive_retry_input,
+    set_footer_prompt, receive_footer_input
+)
 
 # Enable logging
 logging.basicConfig(
@@ -23,7 +37,7 @@ def main():
         logger.error("BOT_TOKEN not found in .env file. Please set it.")
         return
 
-    application = Application.builder().token(bot_token).build()
+    application = ApplicationBuilder().token(bot_token).build()
 
     # Command Handlers
     application.add_handler(CommandHandler("start", start))
@@ -40,39 +54,63 @@ def main():
 
     # Channel Management Handlers
     application.add_handler(CallbackQueryHandler(add_channel_prompt, pattern="^add_channel$"))
-    application.add_handler(MessageHandler(filters.ChatType.CHANNEL | filters.TEXT & filters.User(config.get_admin_ids()), handle_channel_input))
+    application.add_handler(MessageHandler(
+        filters.ChatType.CHANNEL | filters.TEXT & filters.User(config.get_admin_ids()),
+        handle_channel_input
+    ))
     application.add_handler(CallbackQueryHandler(manage_channel, pattern="^channel_\\d+$"))
     application.add_handler(CallbackQueryHandler(remove_channel, pattern="^remove_channel_\\d+$"))
     application.add_handler(CallbackQueryHandler(back_to_channels, pattern="^back_to_channels$"))
 
     # Batch Management Handlers
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) & filters.Context(lambda ctx: ctx.user_data.get("awaiting_batch_message")), collect_message))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) &
+        filters.Context(lambda ctx: ctx.user_data.get("awaiting_batch_message")),
+        collect_message
+    ))
     application.add_handler(CommandHandler("clear_batch", clear_batch))
     application.add_handler(CommandHandler("show_batch", show_batch))
 
     # Schedule Management Handlers
     application.add_handler(CallbackQueryHandler(view_scheduled_posts, pattern="^view_scheduled$"))
     application.add_handler(CallbackQueryHandler(schedule_new_post_prompt, pattern="^schedule_new$"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) & filters.Context(lambda ctx: ctx.user_data.get("awaiting_scheduled_message")), receive_scheduled_message))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) & filters.Context(lambda ctx: ctx.user_data.get("awaiting_scheduled_time")), receive_scheduled_time))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) &
+        filters.Context(lambda ctx: ctx.user_data.get("awaiting_scheduled_message")),
+        receive_scheduled_message
+    ))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) &
+        filters.Context(lambda ctx: ctx.user_data.get("awaiting_scheduled_time")),
+        receive_scheduled_time
+    ))
 
     # Post Management Handlers
     application.add_handler(CommandHandler("preview_post", preview_post))
     application.add_handler(CallbackQueryHandler(execute_post, pattern="^(confirm_post|cancel_post)$"))
 
-
     # Settings Handlers
     application.add_handler(CallbackQueryHandler(set_delay_prompt, pattern="^set_delay$"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) & filters.Context(lambda ctx: ctx.user_data.get("awaiting_delay_input")), receive_delay_input))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) &
+        filters.Context(lambda ctx: ctx.user_data.get("awaiting_delay_input")),
+        receive_delay_input
+    ))
     application.add_handler(CallbackQueryHandler(set_retry_prompt, pattern="^set_retry$"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) & filters.Context(lambda ctx: ctx.user_data.get("awaiting_retry_input")), receive_retry_input))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) &
+        filters.Context(lambda ctx: ctx.user_data.get("awaiting_retry_input")),
+        receive_retry_input
+    ))
     application.add_handler(CallbackQueryHandler(set_footer_prompt, pattern="^set_footer$"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) & filters.Context(lambda ctx: ctx.user_data.get("awaiting_footer_input")), receive_footer_input))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.User(config.get_admin_ids()) &
+        filters.Context(lambda ctx: ctx.user_data.get("awaiting_footer_input")),
+        receive_footer_input
+    ))
 
-    # Run the bot until the user presses Ctrl-C
+    # Run the bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
-
-
